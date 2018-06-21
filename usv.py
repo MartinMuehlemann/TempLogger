c5e1eaaadb3e81dcbdd26dbb5a2b1ba4de5cf3a9
Martin Mühlemann <tinu.muehlemann@gmail.com>
2018-06-21 06:26:41 +0000

    Integrated power-down debouncing

--- usv.py
+++ usv.py
@@ -5,13 +5,20 @@ import time
 import sys
 import os
 import datetime
+import collections
+
 
 SENSORLABEL = "28-000008ab22e6"
 
+GPIO_PFO_PIN = 24
+
+class DebounceException(Exception):
+	pass
+
 def readDS1820(sensorLabel):
-    global tempSensorBezeichnung, tempSensorAnzahl, tempSensorWert, programmStatus
-    x = 0
-    try:
+	global tempSensorBezeichnung, tempSensorAnzahl, tempSensorWert, programmStatus
+	x = 0
+	try:
 		fileName = "/sys/bus/w1/devices/" + sensorLabel + "/w1_slave"
 		file = open(fileName)
 		filecontent = file.read()
@@ -20,8 +27,9 @@ def readDS1820(sensorLabel):
 		stringvalue = filecontent.split("\n")[1].split(" ")[9]
 		sensorwert = float(stringvalue[2:]) / 1000
 		return sensorwert
-	
-    except:
+	except KeyboardInterrupt as kie:
+		raise kie
+	except:
 		print "read DS1820 failed"
 		programmStatus = 0
 		return None
@@ -35,21 +43,87 @@ def gpio_callback_23(channel):
 	capGood = GPIO.input(23) == 1
 	print(u"gpio_callback_23 pfo=%d" % capGood)
 	
+def debounceLevel(lastSamples, threshold):
+	numStableSamples = 0
+	lastValue = lastSamples[0]
+	
+	for pinLevel in lastSamples:
+		if pinLevel == lastValue:
+			numStableSamples += 1
+		else:
+			numStableSamples = 0
+			lastValue = pinLevel
+		if numStableSamples >= threshold:
+			return lastValue
+	
+	raise DebounceException("Unstable signal")
 
+
+def logEvent(msg):
+	t = datetime.datetime.now()
+	timestamp = t.strftime('%d.%m.%Y %H:%M:%S')
+	print("%s %s" %(timestamp, msg))
+
+def getTimeStamp():
+	return datetime.datetime.now().strftime(b'%d.%m.%Y %H:%M:%S')
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(GPIO_PFO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.add_event_detect(23, GPIO.BOTH, callback=gpio_callback_23, bouncetime=10)  
-GPIO.add_event_detect(24, GPIO.BOTH, callback=gpio_callback_24, bouncetime=10)  
+
+
+pfoPinBuffer = collections.deque(maxlen=10)
+debouncedPfoPinLevel = 1 # default power-down
 
 try:
 	while True:
 		temp = readDS1820(SENSORLABEL)
 		if temp != None:
 			print("Temperature %6.2fC" % temp)
+
+
+		pfoPin = GPIO.input(GPIO_PFO_PIN)
+		pfoPinBuffer.appendleft(pfoPin)
+		print(pfoPinBuffer)
+		try:
+			newLevel = debounceLevel(pfoPinBuffer, 5)
+			if newLevel != debouncedPfoPinLevel:
+				debouncedPfoPinLevel = newLevel
+				powerdown = (debouncedPfoPinLevel == 0)
+				
+				timestamp = getTimeStamp()
+									
+				if powerdown:
+					logEvent(u"Power down detected")
+					msg = timestamp + b" Stromausfall bei Kuehlraum detektiert"
+					#GPIO.remove_event_detect(GPIO_T_PIN)
+			
+			# 		print(u"reboot system in 10s...")
+			# 		time.sleep(10)
+			# 		os.system("sudo reboot")
+			# 		while True:
+			# 			time.sleep(1)
+				else:
+					logEvent(u"Power OK")
+					msg = timestamp + b" Stromversorgung bei Kuehlraum wieder OK"
+									
+# 				logEvent(u"SMS: %s" % msg)
+# 				for phoneNumber in SMS_RECIPMENTS:
+# 					try:
+# 				 		sendATCommand(ser, 'AT+CMGS="%s"\r' % phoneNumber)
+# 				 		sendATCommand(ser, msg + chr(26))
+# 						logEvent(u"SMS sent to %s" % phoneNumber)
+# 					except Exception as e:
+# 						logEvent(u"Sending SMS to %s failed. %s" % (phoneNumber, str(e)))
+						
+		except DebounceException as e:
+			# Level at tPin not stable, do not send an SMS
+			logEvent("Pin level unstable")
+			pass
+		
 		time.sleep(1)
 
 except KeyboardInterrupt:  
