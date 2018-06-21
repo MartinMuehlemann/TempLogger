623bfbd3a2c2fb7dff12bfef79d35d98f93515f8
Martin Mühlemann <tinu.muehlemann@gmail.com>
2018-06-21 03:30:45 +0000

    Add system configuration files

---
+++ backupConfig.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DESTINATION_DIR=./system/
+
+mkdir $DESTINATION_DIR
+cp --parents /etc/network/interfaces $DESTINATION_DIR
+cp --parents /etc/dhcpcd.conf $DESTINATION_DIR
+cp --parents /etc/dnsmasq.conf $DESTINATION_DIR
+cp --parents /boot/config.txt $DESTINATION_DIR # 1-Wire
